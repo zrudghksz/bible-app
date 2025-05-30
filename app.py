5ab2fd50a2c246b604c9ab885b7db138c1478e8c
zrudghksz <zrudghksz@gmail.com>
2025-05-30 14:51:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ mode = st.radio("**🎧 모드를 선택하세요**", ["부분 듣기", "전체 
 if mode == "부분 듣기":
     # 1. 안내문구(하얀색) 별도 출력
     st.markdown(
-        "<span style='color:#fff; font-size:1.00em; font-weight:800; display:block; margin-bottom:5px;'>들을 절을 선택하세요.</span>",
+        "<span style='color:#fff; font-size:1.00em; font-weight:800; display:block; margin-bottom:-20px;'>들을 절을 선택하세요.</span>",
         unsafe_allow_html=True
     )
     # 2. selectbox 라벨은 빈 문자열
