566be98845f43c7d68f2d6c47b07c37f2ecefc6c
zrudghksz <zrudghksz@gmail.com>
2025-05-30 17:31:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -213,8 +213,8 @@ elif mode == "부분 암송 테스트":
                 display: inline-block;
                 background: rgba(255,255,255,0.94);
                 color: #14428c;
-                font-size: 1.15em;
-                font-weight: 800;
+                font-size: 1.2em;
+                font-weight: 400;
                 padding: 4px 13px 4px 10px;
                 border-radius: 7px;
                 margin-bottom: 6px;
